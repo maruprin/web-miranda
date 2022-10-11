66a28482894de97bdef2811dcfa26c0ab0cf000f
Maru Di Primio <mariana.diprimio@gmail.com>
2022-10-11 16:00:42 +0200

    lista de hoteles cercanos ok

--- js/googleMaps.js
+++ js/googleMaps.js
@@ -1,25 +1,39 @@
 import hotels from "../data/hotelsUbication.js";
-
+let address;
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 10,
     center: { lat: 41.372232, lng: 2.071841 },
   });
   const icon = {
-    url: "../assets/maps/hotelMirandaUbicationIcon3.png", 
-    scaledSize: new google.maps.Size(25, 40), 
-    origin: new google.maps.Point(0, 0), 
-    anchor: new google.maps.Point(25, 40), 
+    url: "../assets/maps/hotelMirandaUbicationIcon3.png",
+    scaledSize: new google.maps.Size(25, 40),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(25, 40),
   };
   const infoWindow = new google.maps.InfoWindow({
     content: "",
     disableAutoPan: true,
   });
+  const markers = hotels.map((position, i) => {
+    const marker = new google.maps.Marker({
+      position: { lat: position.lat, lng: position.lng },
+      name: position.name,
+      map: map,
+      icon: icon,
+    });
+
+    marker.addListener("click", () => {
+      infoWindow.setContent(marker.name);
+      infoWindow.open(map, marker);
+    });
+    return marker;
+  });
+  const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
 
   //geolocalizacion
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Find actual location";
-  locationButton.classList.add("custom-map-control-button");
+  const currentPosition = []
+  const locationButton = document.getElementById("location");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
   locationButton.addEventListener("click", () => {
     if (navigator.geolocation) {
@@ -29,7 +43,7 @@ function initMap() {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
           };
-
+          currentPosition.push(pos);
           infoWindow.setPosition(pos);
           infoWindow.setContent("You are here");
           infoWindow.open(map);
@@ -52,21 +66,85 @@ function initMap() {
     );
     infoWindow.open(map);
   }
-  const markers = hotels.map((position, i) => {
-    const marker = new google.maps.Marker({
-      position: { lat: position.lat, lng: position.lng },
-      name: position.name,
-      map: map,
-      icon: icon,
+  
+// encontrar ubicacion introducida en input 
+const geocoder = new google.maps.Geocoder();
+  const input = document.getElementById("input-adress");
+  const button = document.getElementById("search-location");
+  button.addEventListener("click", () => {
+   address = input.value;
+    geocoder.geocode({ address: address }, function (results, status) {
+      const pos = results[0].geometry.location;
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("You are here!");
+      infoWindow.open(map);
+      map.setCenter(pos);
     });
-
-    marker.addListener("click", () => {
-      infoWindow.setContent(marker.name);
-      infoWindow.open(map, marker);
-    });
-    return marker;
   });
-  const markerCluster = new markerClusterer.MarkerClusterer({ map, markers });
+
+
+  //locaciones cercanas
+  
+  const nearestButton = document.getElementById("nearest");
+  const wrap = document.getElementById("results-wrap");
+  const results = document.getElementById("results");
+
+  nearestButton.addEventListener("click", () => {
+    console.log('me aprieto')
+    console.log(currentPosition)
+    wrap.classList.add("map__results-active");
+    const destinations = hotels.map((hotel) => ({
+      lat: hotel.lat,
+      lng: hotel.lng,
+    }));
+    if (currentPosition.length) {
+      if (address) {
+        results.innerHTML = "";
+        calculateDistance(address, destinations);
+      } else {
+        const origin = new google.maps.LatLng(
+          currentPosition[0].lat,
+          currentPosition[0].lng
+        );
+        results.innerHTML = "";
+        calculateDistance(origin, destinations);
+      }
+    } else if (address) {
+      results.innerHTML = "";
+      calculateDistance(address, destinations);
+    } else {
+      alert("Define your position");
+    }
+  })
+
+  function calculateDistance(origin, destinations) {
+    var service = new google.maps.DistanceMatrixService();
+    service
+      .getDistanceMatrix({
+        origins: [origin],
+        destinations: destinations,
+        travelMode: "DRIVING",
+      })
+      .then((response) => {
+        const hotels = response.destinationAddresses.map((hotel) => ({
+          name: hotel,
+        }));
+        const distances = response.rows[0].elements.map((dist) => ({
+          distance: dist.distance,
+        }));
+        const sortedHotels = [];
+        for (let i = 0; i < hotels.length; i++) {
+          sortedHotels.push({ ...hotels[i], ...distances[i] });
+        }
+        sortedHotels.sort((a, b) => a.distance.value - b.distance.value);
+        for (let hotel of sortedHotels) {
+          const distance = document.createElement("li");
+          distance.innerText = `${hotel.name} - ${hotel.distance.text}`;
+          document.getElementById("results").appendChild(distance);
+        }
+      });
+  }
+  
 }
 
 window.initMap = initMap;
